50856f72e7707b53deed8ba65779f550a741db78
Sulthan Ahmad <Sulthanahmad41@gmail.com>
2023-05-24 21:55:57 +0700

    title user ekstra admin ekstra

--- resources/views/adminPages/adminekstrakurikuler.blade.php
+++ resources/views/adminPages/adminekstrakurikuler.blade.php
@@ -14,7 +14,7 @@
       <div class="container">
 
         <div class="d-flex justify-content-between align-items-center">
-          <h2>Siswa</h2>
+          <h2>Ekstrakurikuler</h2>
           <ol>
             <li><a href="/">Beranda</a></li>
             <li><a href="/siswa">Daftar Siswa Aktif</a></li>
