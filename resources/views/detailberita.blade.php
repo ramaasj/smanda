37543260aa9e8173d5ee13ba461057f886d8b00e
Tenzilla <zilla.titann@gmail.com>
2023-05-28 17:36:36 +0700

    tanpa waktu

--- resources/views/detailberita.blade.php
+++ resources/views/detailberita.blade.php
@@ -10,60 +10,60 @@
 
 <main id="main">
 
-    <!-- ======= Breadcrumbs Section ======= -->
-    <section class="breadcrumbs">
-      <div class="container">
+  <!-- ======= Breadcrumbs Section ======= -->
+  <section class="breadcrumbs">
+    <div class="container">
 
-        <div class="d-flex justify-content-between align-items-center">
-          <h2>Detail Berita</h2>
-          <ol>
-            <li><a href="/">Beranda</a></li>
-            <li><a href="/berita">Berita</a></li>
-          </ol>
-        </div>
-
+      <div class="d-flex justify-content-between align-items-center">
+        <h2>Detail Berita</h2>
+        <ol>
+          <li><a href="/">Beranda</a></li>
+          <li><a href="/berita">Berita</a></li>
+        </ol>
       </div>
-    </section><!-- Breadcrumbs Section -->
 
-    <!-- ======= Portfolio Details Section ======= -->
-    <section id="portfolio-details" class="portfolio-details">
-      <div class="container">
+    </div>
+  </section><!-- Breadcrumbs Section -->
 
-        <div class="row gy-4">
+  <!-- ======= Portfolio Details Section ======= -->
+  <section id="portfolio-details" class="portfolio-details">
+    <div class="container">
 
-          <div class="col-lg-8">
-            <div class="portfolio-details-slider swiper">
-              <div class="swiper-wrapper align-items-center">
+      <div class="row gy-4">
 
-                <div class="swiper">
-                  <img src="{{asset($berita -> foto_berita)}}" alt="">
-                </div>
+        <div class="col-lg-8">
+          <div class="portfolio-details-slider swiper">
+            <div class="swiper-wrapper align-items-center">
 
+              <div class="swiper">
+                <img src="{{asset($berita -> foto_berita)}}" alt="">
               </div>
-              <div class="swiper-pagination"></div>
-            </div>
-          </div>
 
-          <div class="col-lg-4">
-            <div class="portfolio-info">
-              <h3>Terkait Berita Ini</h3>
-              <ul>
-                <li><strong>Kategori</strong>: {{$berita -> kategori}}</li>
-                <li><strong>Tanggal</strong>: {{$berita -> created_at}}</li>
-              </ul>
             </div>
-            <div class="portfolio-description">
-              <h2>{{$berita -> judul}}</h2>
-              <p>
-                <p> {{$berita -> description}} </p>
-              </p>
-            </div>
+            <div class="swiper-pagination"></div>
           </div>
+        </div>
 
+        <div class="col-lg-4">
+          <div class="portfolio-info">
+            <h3>Terkait Berita Ini</h3>
+            <ul>
+              <li><strong>Kategori</strong>: {{$berita -> kategori}}</li>
+              <li><strong>Tanggal</strong>: {{$berita->created_at->format('Y-m-d')}}</li>
+            </ul>
+          </div>
+          <div class="portfolio-description">
+            <h2>{{$berita -> judul}}</h2>
+            <p>
+            <p> {{$berita -> description}} </p>
+            </p>
+          </div>
         </div>
 
       </div>
-    </section><!-- End Portfolio Details Section -->
 
-  </main><!-- End #main -->
+    </div>
+  </section><!-- End Portfolio Details Section -->
+
+</main><!-- End #main -->
 @endsection
